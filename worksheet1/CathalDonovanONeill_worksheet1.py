607f1c0cbc7950c820b78a4fd7afc1a703e9a2b7
cathaldon1 <43860317+cathaldon1@users.noreply.github.com>
2018-11-24 12:54:31 +0000

    Add files via upload

---
+++ worksheet1/CathalDonovanONeill_worksheet1.py
@@ -0,0 +1,12 @@
+age = input("What age are you?")
+ 
+if age < 12:
+	print("You can watch movies rated G, PG, and 12A and 15A (but only accompanied by an adult.")
+elif age = 12:
+	print ("You can watch movies rated G, PG, 12A and 15A (but only accompanied by an adult.")
+elif age < 16:
+	print("You can watch movies rated G, PG, 12A and 15A.")
+elif age <18:
+	print("You can watch movies rated G, PG, 12A, 15A and 16.")
+else:
+	print("You can watch movies rated G, PG, 12A, 15A, 16 and 18.")
